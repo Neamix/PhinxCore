72a1be1098f0da0d2873532e753cc8f9d4d10eee
NEAMIX\abdal <abdalrhmanhussin44@gmail.com>
2022-06-24 06:26:26 +0200

    add .env.example

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -2,56 +2,7 @@
 
 @section('content')
 <div class="chart mt-4 container-fluid">
-  <div class="row">
-    <div class="col-md-12">
-      <div class="card mt-4">
-        <div class="card-header w-100 d-flex">
-            <h4 class="card-title mb-1">Tour</h4>
-        </div>
-        <div class="card-content">
-          <div class="tour container">
-
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="col-md-4">
-      <div class="card mt-4">
-        <div class="card-header w-100 d-flex">
-            <h4 class="card-title mb-1">Destination</h4>
-        </div>
-        <div class="card-content">
-          <div class="destination container">
-
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="col-md-4">
-      <div class="card mt-4">
-        <div class="card-header w-100 d-flex">
-            <h4 class="card-title mb-1">Hotel</h4>
-        </div>
-        <div class="card-content">
-          <div class="hotel container">
-
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="col-md-4">
-      <div class="card mt-4">
-        <div class="card-header w-100 d-flex">
-            <h4 class="card-title mb-1">Blog</h4>
-        </div>
-        <div class="card-content">
-          <div class="blog container">
-
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+  
 </div>
 @endsection
 
--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Auth::routes();
+
+Route::get('/',function(){
+    return view('welcome');
+});
