fb3d47370b6c8bb2318a38c60b6e0f61e4fe828f
NEAMIX\abdal <abdalrhmanhussin44@gmail.com>
2022-06-24 06:26:12 +0200

    fix routes table doesnt exist  bug

--- app/Helpers/Sonamak.php
+++ app/Helpers/Sonamak.php
@@ -6,37 +6,41 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 use App\Http\Admin\PostController;
 use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\Schema;
 
 class Sonamak {
 
     static function routes()
     {
-        $routes = DB::table('routes')->get();
+        $route_table_exist = Schema::hasTable('routes');
 
-        foreach( $routes as $route ) {
+        if ( $route_table_exist ) {
+            $routes = DB::table('routes')->get();
 
-            $name  = $route->name;
-            $lowername = strtolower($name);
-            $type = $route->type;
-
-            Route::group(['prefix' => $lowername], function() use ($name,$type,$lowername) {
-
-                $uppername = ucwords($name);
-                $controller = "Admin\\".$uppername."Controller";
-
-                Route::get("/","$controller@index")->name("$lowername");
-                Route::post("/store","$controller@store")->name("$lowername.store");
-                Route::post("/more","$controller@more")->name("$lowername.more");
-                Route::post('/delete/'.'{'.$lowername.'}',"$controller@delete")->name("$lowername.delete");
-                
-
-                if ( $type == 'multi' ) {
-                    Route::get('/upsert/'.'{'.$lowername.'?}',"$controller@upsert")->name("$lowername.upsert");
-                }
-                
-            });
-
+            foreach( $routes as $route ) {
+    
+                $name  = $route->name;
+                $lowername = strtolower($name);
+                $type = $route->type;
+    
+                Route::group(['prefix' => $lowername], function() use ($name,$type,$lowername) {
+    
+                    $uppername = ucwords($name);
+                    $controller = "Admin\\".$uppername."Controller";
+                    Route::get("/","$controller@index")->name("$lowername");
+                    Route::post("/store","$controller@store")->name("$lowername.store");
+                    Route::post("/more","$controller@more")->name("$lowername.more");
+                    Route::post('/delete/'.'{'.$lowername.'}',"$controller@delete")->name("$lowername.delete");
+    
+    
+                    if ( $type == 'multi' ) {
+                        Route::get('/upsert/'.'{'.$lowername.'?}',"$controller@upsert")->name("$lowername.upsert");
+                    }
+    
+                    
+                });
+    
+            }   
         }
-
     }
 }
